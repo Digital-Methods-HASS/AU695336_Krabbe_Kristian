7ff5f2323a48f974a90c766e4beb6c0e95d47114
KristianKrabbe <144119407+KristianKrabbe@users.noreply.github.com>
2023-12-14 16:20:26 +0100

    Add files via upload

---
+++ Homework/Week 39/W39_Homework_Kongerækken.R
@@ -0,0 +1,21 @@
+
+library(tidyverse)
+read_csv("data/Dansk kongerække - Ark1.csv")
+
+Kongerækken <- read_csv("data/Dansk kongerække - Ark1.csv")
+
+Kongerækken <- Kongerækken %>%
+  mutate(Regeringstid = Regeringslut - Regeringsstart)
+
+colnames(Kongerækken)
+
+Kongerækken %>% 
+  ggplot(aes(x = Regeringsstart, y = Regeringstid))+
+  geom_point()+
+  geom_smooth()+
+  geom_line()+
+  labs(x = "Tid",
+       y = "Regeringstid")
+ggsave("Kongerækken_visualisering.png")
+
+
